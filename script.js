8660138bb973039724a7e1725a4366c0a941c758
Suman Biswas <indianvloggersuman@gmail.com>
2026-03-19 07:21:51 -0300

    sistema grid foi atualizado em pagina de dashboard.html

---
+++ script.js
@@ -0,0 +1,37 @@
+const ctx = document.getElementById('inventoryChart');
+
+new Chart(ctx, {
+  type: 'bar', // line chart (best for date-wise data)
+
+  data: {
+    labels: [
+      'Mar 1', 'Mar 2', 'Mar 3', 'Mar 4', 'Mar 5'
+    ], // dates
+
+    datasets: [
+      {
+        label: 'Stock In',
+        data: [10, 20, 15, 30, 25],
+        borderColor: 'green',
+        backgroundColor: 'green',
+        tension: 0.4
+      },
+      {
+        label: 'Stock Out',
+        data: [5, 15, 10, 20, 18],
+        borderColor: 'red',
+        backgroundColor: 'red',
+        tension: 0.4
+      }
+    ]
+  },
+
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }
+});
